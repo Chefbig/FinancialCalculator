c9d37c296480f871c1e67d29fab612e808f9aaf5
tonghuang <thuangster@gmail.com>
2015-02-24 16:51:38 -0500

    adding UI for basic calculator

--- app/src/main/java/com/thuangster/financialcalculator/MyActivity.java
+++ app/src/main/java/com/thuangster/financialcalculator/MyActivity.java
@@ -1,23 +1,28 @@
 package com.thuangster.financialcalculator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MyActivity extends Activity {
-    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+    private double total = 0;
+    private String operation;
+    private int placeValue = 0;
+    private static final String CLEAR_BUTTON_TEXT = "CE | C";
+    private static final String POS_NEG_BUTTON_TEXT = "+ | -";
+    public static final String EXTRA_MESSAGE = "This is a help message.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
     }
-// just a test
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,11 +46,44 @@ public class MyActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void sendMessage(View view) {
+    public void onButtonClick(View view) {
+        Button button = (Button) view;
+        String bText = button.getText().toString();
+        if (isInteger(bText, 10)) {
+            int value = Integer.parseInt(bText);
+            if (placeValue == 0) {
+                total = total + value;
+            } else {
+                total = 10 * total + value;
+            }
+
+            placeValue++;
+        } else if (bText.equals(CLEAR_BUTTON_TEXT)) {
+            total = 0;
+            placeValue = 0;
+        } else if (bText.equals(POS_NEG_BUTTON_TEXT)) {
+            total = total * -1;
+        }
+        TextView myTextView = (TextView) findViewById(R.id.textView);
+        myTextView.setText(Double.toString(total));
+
+    }
+
+    public void helpMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putExtra(EXTRA_MESSAGE, EXTRA_MESSAGE);
         startActivity(intent);
+    }
+
+    private static boolean isInteger(String s, int radix) {
+        if(s.isEmpty()) return false;
+        for(int i = 0; i < s.length(); i++) {
+            if(i == 0 && s.charAt(i) == '-') {
+                if(s.length() == 1) return false;
+                else continue;
+            }
+            if(Character.digit(s.charAt(i),radix) < 0) return false;
+        }
+        return true;
     }
 }
